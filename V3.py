6c1178c6b961735f38a930c43dc211c2e8593293
SigursteinnOli <34093550+SigursteinnOli@users.noreply.github.com>
2018-09-19 12:25:39 +0000

    Update V3.py

--- V3.py
+++ V3.py
@@ -2,6 +2,7 @@
 #10/09/18
 #Verkefni 3
 
+from sys import argv
 from bottle import *
 
 @route("/")
@@ -45,7 +46,7 @@ def villa(error):
     return "<h1 style = color:red>Þessi síða finnst ekki</h1>"
 
 
-run(host="localhost", port =8080, reloader=True, debug=True)
-
+#run(host="localhost", port =8080, reloader=True, debug=True)
+run(host='0.0.0.0', port=oa.environ.get('PORT'))
 
 
